8162ba7c23428be8b58823fdbe6f8b6091dc4186
Cat-JiYoung <kimyamae82@gmail.com>
2023-11-16 22:30:39 +0900

    [FEAT] Image, Card, State

--- app/src/main/java/com/jiyoungmolcompose/MainActivity.kt
+++ app/src/main/java/com/jiyoungmolcompose/MainActivity.kt
@@ -3,44 +3,85 @@ package com.jiyoungmolcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            /*
-            [일반 Column 사용]
-             val scrollState = rememberScrollState()
-            Column(modifier = Modifier.background(Color.Green).fillMaxWidth().verticalScroll(scrollState)) {
-                for (i in 1..50) {
-                    Text("글씨 $i")
-                }
+            var isFavorite by rememberSaveable {
+                mutableStateOf(false)
             }
-             */
-            LazyColumn(
+            ImageCard(
                 modifier = Modifier
-                    .background(Color.Green)
-                    .fillMaxWidth(),
-                contentPadding = PaddingValues(16.dp), // modifier에 정의해줘도 됨
-                verticalArrangement = Arrangement.spacedBy(4.dp), // 아이템 간의 간격을 줌
+                    .background(Color.Black)
+                    .fillMaxWidth(0.5f)
+                    .padding(16.dp),
+                isFavorite = isFavorite,
+            ) { favorite ->
+                isFavorite = favorite
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    modifier: Modifier = Modifier,
+    isFavorite: Boolean,
+    onTabFavorite: (Boolean) -> Unit,
+) {
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 28.dp,
+        ),
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(id = R.drawable.mike2),
+                contentDescription = "poster",
+                contentScale = ContentScale.Crop, // scaleType
+            )
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.TopEnd,
             ) {
-                item {
-                    Text("Header")
-                }
-                items(50) { index ->
-                    Text("글씨 $index")
-                }
-                item {
-                    Text("Footer")
+                IconButton(onClick = {
+                    onTabFavorite(!isFavorite)
+                }) {
+                    Icon(
+                        imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                        contentDescription = "favorite",
+                        tint = Color.White,
+                    )
                 }
             }
         }
