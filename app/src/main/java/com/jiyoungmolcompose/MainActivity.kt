c08ec01ae83543fefbd4ef50e8ca4e3c883d2e8c
Cat-JiYoung <kimyamae82@gmail.com>
2023-11-16 21:09:06 +0900

    [FEAT] List, LazyColumn

--- app/src/main/java/com/jiyoungmolcompose/MainActivity.kt
+++ app/src/main/java/com/jiyoungmolcompose/MainActivity.kt
@@ -4,58 +4,45 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.jiyoungmolcompose.ui.theme.MolComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(
+            /*
+            [일반 Column 사용]
+             val scrollState = rememberScrollState()
+            Column(modifier = Modifier.background(Color.Green).fillMaxWidth().verticalScroll(scrollState)) {
+                for (i in 1..50) {
+                    Text("글씨 $i")
+                }
+            }
+             */
+            LazyColumn(
                 modifier = Modifier
-                    .background(Color.Green).fillMaxWidth().height(200.dp),
+                    .background(Color.Green)
+                    .fillMaxWidth(),
+                contentPadding = PaddingValues(16.dp), // modifier에 정의해줘도 됨
+                verticalArrangement = Arrangement.spacedBy(4.dp), // 아이템 간의 간격을 줌
             ) {
-                Text(text = "Hello")
-                Box(
-                    modifier = Modifier.fillMaxSize()
-                        .padding(16.dp),
-                    contentAlignment = Alignment.BottomEnd,
-                ) {
-                    Text(text = "Hoho")
+                item {
+                    Text("Header")
+                }
+                items(50) { index ->
+                    Text("글씨 $index")
+                }
+                item {
+                    Text("Footer")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text("Hello $name!")
-}
-
-@Preview(showBackground = false)
-@Composable
-fun DefaultPreview() {
-    MolComposeTheme {
-        Greeting("지영")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview2() {
-    MolComposeTheme {
-        Greeting("마이크")
     }
 }
