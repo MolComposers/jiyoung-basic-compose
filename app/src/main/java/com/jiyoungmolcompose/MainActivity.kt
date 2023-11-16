1353b941cf90111cee31bc9e7966610bdb7ca648
Cat-JiYoung <kimyamae82@gmail.com>
2023-11-16 20:36:50 +0900

    [FEAT] Box

--- app/src/main/java/com/jiyoungmolcompose/MainActivity.kt
+++ app/src/main/java/com/jiyoungmolcompose/MainActivity.kt
@@ -4,18 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jiyoungmolcompose.ui.theme.MolComposeTheme
@@ -24,21 +22,17 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MolComposeTheme {
-                Surface(
-                    color = MaterialTheme.colorScheme.background,
+            Box(
+                modifier = Modifier
+                    .background(Color.Green).fillMaxWidth().height(200.dp),
+            ) {
+                Text(text = "Hello")
+                Box(
+                    modifier = Modifier.fillMaxSize()
+                        .padding(16.dp),
+                    contentAlignment = Alignment.BottomEnd,
                 ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(16.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.SpaceBetween,
-                    ) {
-                        Text("Hello!!!")
-                        Spacer(modifier = Modifier.width(16.dp))
-                        Text("World")
-                    }
+                    Text(text = "Hoho")
                 }
             }
         }
